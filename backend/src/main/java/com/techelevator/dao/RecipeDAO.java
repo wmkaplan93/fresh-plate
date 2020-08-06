e0a9e4c9a008e52d16ac46034f68d95f048f6024
Alexa Kostelnik <adkostelnik4@gmail.com>
2020-08-06 12:21:44 -0400

    More DAO work

--- backend/src/main/java/com/techelevator/dao/RecipeDAO.java
+++ backend/src/main/java/com/techelevator/dao/RecipeDAO.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.techelevator.model.Recipe;
 import com.techelevator.model.RecipeIngredient;
+import com.techelevator.model.RecipeType;
 
 public interface RecipeDAO {
 	 List<Recipe> findAllPublicRecipes();
@@ -18,12 +19,16 @@ public interface RecipeDAO {
 	 
 	 boolean deleteRecipe(long recipeId);
 	 
-	 List<Recipe> findRecipesByType (String type, String userName);
+	 List<Recipe> findRecipesByType (String type);
 	 
 	 List<Recipe> findRecipesByUser (String userName);
 	 
 	 List<Recipe> findFavoriteRecipesByUser (String userName);
 	 
 	 List<RecipeIngredient> getIngredientsByRecipeId (long recipeId);
+	 
+	 RecipeType recipeTypes(long recipeId);
+	 
+	 List<Recipe> findRecipesByTypeAndUsername (String type, String userName);
 
 }
---
+++ backend/src/main/java/com/techelevator/model/RecipeType.java
@@ -0,0 +1,21 @@
+package com.techelevator.model;
+
+public class RecipeType {
+	
+	private String type;
+	
+	public RecipeType() {}
+
+	public RecipeType(String type) {
+		this.type = type;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
